1cbeb76b70cf2aab846f532f7029aa03a1041e0f
baouz06 <baouz.khaled@gmail.com>
2017-04-06 17:28:27 +0100

    mise en oeuvre d'angular Js pour tp 4

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tp7sirApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the tp7sirApp
+ */
+angular.module('tp7sirApp')
+  .controller('MainCtrl', function ($scope,$http) {
+  
+    $scope.kh="abouzzuozz";
+    // recupération des données via $http
+	$http.get("http://localhost:9000/rest/person/").then(function(response) {
+		$scope.persons = response.data;
+	});
+  });
